45271c93d167de1903a049d7a3a7235324e0dc99
Kaito Suehiro <baseballscore.info@gmail.com>
2022-07-15 22:05:16 +0900

    pwa 修正

--- public/sw.js
+++ public/sw.js
@@ -14,41 +14,64 @@
 //     ]
 // }]
 
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    // キャッシュ化したいコンテンツ
-];
+// var CACHE_NAME = 'pwa-sample-caches';
+// var urlsToCache = [
+//     // キャッシュ化したいコンテンツ
+// ];
+
+// self.addEventListener('install', function(event) {
+//     console.log('sw event: install called');
+
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//         .then(function(cache) {
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', function(event) {
+//     console.log('sw event: fetch called');
+
+//     event.respondWith(
+//         caches.match(event.request)
+//         .then(function(response) {
+//             return response ? response : fetch(event.request);
+//         })
+//     );
+// });
+
+// self.addEventListener('push', function(event) {
+//     console.log('sw event: push called');
+
+//     var notificationDataObj = event.data.json();
+//     var content = {
+//         body: notificationDataObj.body,
+//     };
+//     event.waitUntil(
+//         self.registration.showNotification(notificationDataObj.title, content)
+//     );
+// });
+
+
+const le = true;
+if (le) console.log('Service Worker Start');
+
+if (le) console.log('import workbox');
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
+
+if (le) console.log('skipWaiting');
+workbox.core.skipWaiting();
+
+if (le) console.log('clientsClaim');
+workbox.core.clientsClaim();
 
 self.addEventListener('install', function(event) {
-    console.log('sw event: install called');
-
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+    if (le) console.log('Service Worker Install', event);
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log('sw event: fetch called');
-
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            return response ? response : fetch(event.request);
-        })
-    );
+self.addEventListener('activate', function(event) {
+    if (le) console.log('Service Worker Activate', event);
 });
 
-self.addEventListener('push', function(event) {
-    console.log('sw event: push called');
-
-    var notificationDataObj = event.data.json();
-    var content = {
-        body: notificationDataObj.body,
-    };
-    event.waitUntil(
-        self.registration.showNotification(notificationDataObj.title, content)
-    );
-});
+if (le) console.log('Service Worker End');

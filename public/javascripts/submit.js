098c9867b3ae46292d1c73df1b35906b030f2b90
Kaito Suehiro <baseballscore.info@gmail.com>
2022-07-15 22:40:26 +0900

    pwa修正２

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -227,10 +227,10 @@ function changePositionFlg() {
     changeFlag.value = 1;
 };
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(async(registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch((err) => {
-        console.log('ServiceWorker registration failed: ', err);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw.js').then(async(registration) => {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }).catch((err) => {
+//         console.log('ServiceWorker registration failed: ', err);
+//     });
+// }
